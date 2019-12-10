12ad047bd16d7cbe131dee6ee599fcff78672556
YouOldMaid <karen.huaulme@gmail.com>
2019-12-10 12:31:25 -0600

    Update source.js

--- Challenge/functions/ChallengeNotify/source.js
+++ Challenge/functions/ChallengeNotify/source.js
@@ -1,28 +1,15 @@
 exports = function(changeEvent) {
   
   console.log("IN CHALLENGE NOTIFY FUNCTION!");
-  const db = context.services.get("mongodb-atlas").db("sample_training");
-  const companies = db.collection("companies");
   
   const TwilioService = context.services.get("TwilioReInventChallenge");
   
   var text = `${context.values.get("Competitor")} configured the trigger correctly!`;
- 
-  // TwilioService.send({
-  //   to: "+16126007305",
-  //   from: context.values.get("TwilioNumber"),
-  //   body: text
-  // });
- 
-  // TwilioService.send({
-  //   to: "+16504836372",
-  //   from: context.values.get("TwilioNumber"),
-  //   body: text
-  // });
+
   
  
  TwilioService.send({
-    to: "+15124230644",
+    to: "+15124230644",       // ENTER YOUR PHONE NUMBER HERE
     from: context.values.get("TwilioNumber"),
     body: text
   });
